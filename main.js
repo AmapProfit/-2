a2192aec26f92e9af2250e76c5e4632c04bd4d46
Danchich-man <Danman70@yandex.ru>
2024-06-29 11:51:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,18 @@ document.addEventListener('DOMContentLoaded', () => {
     const energyCountDisplay = document.getElementById('energy-count');
     const energyBar = document.getElementById('energy-bar');
 
+    const boostsIcon = document.getElementById('boosts-icon');
+    const boostsMenu = document.getElementById('boosts-menu');
+    const backIconBoosts = document.getElementById('back-icon-boosts');
+
+    const friendsIcon = document.getElementById('friends-icon');
+    const friendsMenu = document.getElementById('friends-menu');
+    const backIconFriends = document.getElementById('back-icon-friends');
+
+    const airdropIcon = document.getElementById('airdrop-icon');
+    const airdropMenu = document.getElementById('airdrop-menu');
+    const backIconAirdrop = document.getElementById('back-icon-airdrop');
+
     let clayCount = 0;
     let energy = 5000;
     const maxEnergy = 5000;
@@ -17,6 +29,30 @@ document.addEventListener('DOMContentLoaded', () => {
             energy -= energyPerClick;
             updateDisplays();
         }
+    });
+
+    boostsIcon.addEventListener('click', () => {
+        boostsMenu.classList.add('show');
+    });
+
+    backIconBoosts.addEventListener('click', () => {
+        boostsMenu.classList.remove('show');
+    });
+
+    friendsIcon.addEventListener('click', () => {
+        friendsMenu.classList.add('show');
+    });
+
+    backIconFriends.addEventListener('click', () => {
+        friendsMenu.classList.remove('show');
+    });
+
+    airdropIcon.addEventListener('click', () => {
+        airdropMenu.classList.add('show');
+    });
+
+    backIconAirdrop.addEventListener('click', () => {
+        airdropMenu.classList.remove('show');
     });
 
     function updateDisplays() {
